35408834f92350f57d245449cf458b1cce663a76
xuech <xuech_love@126.com>
2020-10-22 11:39:48 +0800

    @feat 提交验证码接口

--- src/routes/loginRouter.js
+++
@@ -1,9 +0,0 @@
-// import Router from 'koa-router'
-// import loginController from '@/api/LoginController'
-
-// const router = new Router()
-// router.prefix('/login')
-
-// router.post('/login', loginController.login)
-
-// export default router
--- src/routes/publicRouter.js
+++ src/routes/publicRouter.js
@@ -1,10 +1,8 @@
-// import Router from 'koa-router'
-// import publicController from '../api/PublicController'
+import Router from 'koa-router'
+import publicController from '../api/PublicController'
 
-// const router = new Router()
+const router = new Router()
 
-// router.prefix('/public')
+router.get('/getCaptcha', publicController.getCaptcha)
 
-// router.get('/getCaptcha', publicController.getCaptcha)
-
-// export default router
+export default router
